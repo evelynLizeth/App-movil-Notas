1e268d8e5182e2fb6d01d3c0e097db8e931ffa4b
Evelyn <evelizeth@outlook.es>
2025-12-30 22:47:03 -0500

    feat: Obtiene el promedio en la pantalla lista de notas

--- app/src/main/java/com/eve/notas/data/model/Grade.kt
+++ app/src/main/java/com/eve/notas/data/model/Grade.kt
@@ -3,12 +3,13 @@ package com.eve.notas.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "grade")
-data class Grade( @PrimaryKey(autoGenerate = true) val id: Long = 0,
-
-                  val pinned: Boolean = false,
-                  val studentId: Long,
-                  val taskId: Long,
-                  val value: Double
-
+@Entity(
+    tableName = "grade",
+    primaryKeys = ["studentId", "taskId"]
+)
+data class Grade(
+    val studentId: Long,
+    val taskId: Long,
+    val value: Double,
+    val pinned: Boolean = false
 )
